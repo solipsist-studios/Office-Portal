a1d81b10d64f80197bec90071f3b03d4ad3b5470
Jeff Sipko <jeff@solipsist.studio>
2023-06-05 20:50:47 -0400

    Added AirBnB scene

--- Assets/Scripts/SceneAnchorController.cs
+++ Assets/Scripts/SceneAnchorController.cs
@@ -1,20 +1,87 @@
 // Copyright (c) Solipsist Studios Inc.
 // Licensed under the MIT License.
 
+using Microsoft.Azure.SpatialAnchors;
+using Microsoft.Azure.SpatialAnchors.Unity;
+using System.Collections.Generic;
+
 using Unity.Netcode;
 using UnityEngine;
 using UnityEngine.XR.Interaction.Toolkit;
+
+using Solipsist;
+using Solipsist.Models;
+
 
 public class SceneAnchorController : NetworkBehaviour
 {
     //[SerializeField] private NetworkObject parentNetworkObject = null;
     //[SerializeField] private GameObject sceneVisuals = null; // Root of physical/visible object
+    
+    private AzureSpatialAnchors spatialAnchorManager = null;
     private ulong? ownerId = null;
+    private bool isSceneAnchorSet = false;
+
+    // Handily, we only have anchors on AR platforms, where we DO want to disable the bg
+    // Probably need to make this more robust in the future though...
+    [SerializeField] private List<GameObject> disableWhenAnchored = new List<GameObject>();
+
     //public NetworkVariable<Vector3> HeadPosition = new NetworkVariable<Vector3>(
     //        default,
     //        NetworkVariableReadPermission.Everyone,
     //        NetworkVariableWritePermission.Owner
     //    );
+    
+    public void SetSceneAnchor()
+    {
+        if (this.isSceneAnchorSet)
+        {
+            return;
+        }
+
+        OnAnchorSet();
+
+#if UNITY_WSA || UNITY_ANDROID
+        UnityDispatcher.InvokeOnAppThread(async () =>
+        {
+            await this.spatialAnchorManager.AnchorObjectAsync(this.gameObject, new AnchorObjectModel());
+        });
+#endif
+    }
+
+    private void Start()
+    {
+        this.spatialAnchorManager = FindObjectOfType<AzureSpatialAnchors>();
+
+        if (this.spatialAnchorManager == null)
+        {
+            Debug.LogError("SceneAnchorController requires an AzureSpatialAnchors component to exist in the scene!");
+            return;
+        }
+
+        this.spatialAnchorManager.AnchorLocatedCallback += SpatialAnchorManager_AnchorLocatedCallback;
+    }
+
+    private void SpatialAnchorManager_AnchorLocatedCallback(AnchorObjectModel model, AnchorLocatedEventArgs eventArgs)
+    {
+        // Attach the anchor components
+        Pose anchorPose = eventArgs.Anchor.GetPose();
+        this.transform.SetPositionAndRotation(anchorPose.position, anchorPose.rotation);
+
+        this.spatialAnchorManager.AnchorGameObject(this.gameObject, eventArgs.Anchor);
+
+        OnAnchorSet();
+    }
+
+    private void OnAnchorSet()
+    {
+        // TODO: Change to "Update Anchor"
+        this.isSceneAnchorSet = true;
+        foreach (GameObject obj in this.disableWhenAnchored)
+        {
+            obj.SetActive(false);
+        }
+    }
 
     private void Update()
     {
